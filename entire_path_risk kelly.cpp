3f2034acc0a716f3c1eb10a982364a9a052bead3
KellyChen520 <45199466+KellyChen520@users.noreply.github.com>
2018-11-24 18:04:21 +0800

    Update entire_path_risk kelly.cpp

--- entire_path_risk kelly.cpp
+++ entire_path_risk kelly.cpp
@@ -13,63 +13,78 @@ float totalLineRisk (int *allPoints, int *X, int *Y, int *R, int *P, int m, int 
 	
 	for(int i =0; i < 2*k + 4; i += 2)
 	{
-	        float length = sqrt(pow(allPoints[i] - allPoints[i+2],2) + pow(allPoints[i+1] - allPoints[i+3],2));
-	        
-	        int times = static_cast<int>(length);   //轉折點個數 
-	        
-	        if(i == 0 )
-	        {
-	        	Xnew = allPoints[i];
-	        	Ynew = allPoints[i+1];
-		}
-		else
+		if(allPoints[i] != -1)
 		{
-			Xstart = allPoints[i];
-			Ystart = allPoints[i+1];
-			Xend = allPoints[i+2];
-			Yend = allPoints[i+3];
-				
-			float lengthInt = static_cast<float>(times);   
-	                float restLenghth = 1 - (length - lengthInt);   //下一個起點離轉折點的距離 
-	            
-	                Xnew = newStart(restLenghth, Xstart,  Xend);   //新的起點的x座標 
-	                Ynew = newStart(restLenghth, Ystart,  Yend);   //新的起點的y座標
-		}
+	        	float length = sqrt(pow(allPoints[i] - allPoints[i+2],2) + pow(allPoints[i+1] - allPoints[i+3],2));
 	        
-	        if(times == length)
-		    times -= 1;
-		    
-	        if(Xnew != allPoints[0] && Ynew != allPoints[1])
-		{
-		        for(int b = 0; b < m; b++)//bomb num
+	        	int times = static_cast<int>(length);   //分割點個數 
+	        
+			if(i == 0 )
 			{
-				float d = sqrt(pow((Xnew - X[b]), 2) + pow((Ynew - Y[b]), 2));   //分割點和炸彈的距離 
-						
-				if(R[b] - d > 0)
+				Xnew = allPoints[i];
+				Ynew = allPoints[i+1];
+
+				if(allPoints[i+2] != -1)
 				{
-					eachPathRisk += P[b] * (R[b] - d) / R[b];
+					Xend = allPoints[i+2];
+					Yend = allPoints[i+3];
 				}
-							
-			} 
-		}
+				else
+				{
+					Xend = allPoints[2*k + 2];
+					Yend = allPoints[2*k + 3];
+				}
+
+			}
+			else
+			{
+				Xstart = allPoints[i];
+				Ystart = allPoints[i+1];
+				Xend = allPoints[i+2];
+				Yend = allPoints[i+3];
+
+				float lengthInt = static_cast<float>(times);   
+				float restLenghth = 1 - (length - lengthInt);   //下一個起點離轉折點的距離 
+
+				Xnew = newStart(restLenghth, Xstart,  Xend);   //新的起點的x座標 
+				Ynew = newStart(restLenghth, Ystart,  Yend);   //新的起點的y座標
+			}
+
+			if(times == length)
+			    times -= 1;
+
+			if(Xnew != allPoints[0] && Ynew != allPoints[1])
+			{
+				for(int b = 0; b < m; b++)//bomb num
+				{
+					float d = sqrt(pow((Xnew - X[b]), 2) + pow((Ynew - Y[b]), 2));   //分割點和炸彈的距離 
+
+					if(R[b] - d > 0)
+					{
+						eachPathRisk += P[b] * (R[b] - d) / R[b];
+					}
+
+				} 
+			}
 		    
 	 
-	        for(int t = 1; t <= times; t++)//dot num
-	        {
-			float x = Xnew + (Xend-Xnew) / length * t;  //下一個的x座標 
-			float y = Ynew + (Yend-Ynew) / length * t;  //下一個的y座標 
+	        	for(int t = 1; t <= times; t++)//dot num
+	        	{
+				float x = Xnew + (Xend-Xnew) / length * t;  //下一個的x座標 
+				float y = Ynew + (Yend-Ynew) / length * t;  //下一個的y座標 
 					
-			for(int b = 0; b < m; b++)//bomb num
-			{
-				float d = sqrt(pow((x - X[b]), 2) + pow((y - Y[b]), 2));   //分割點和炸彈的距離 
-						
-				if(R[b] - d > 0)
+				for(int b = 0; b < m; b++)//bomb num
 				{
-					eachPathRisk += P[b] * (R[b] - d) / R[b];
-				}
+					float d = sqrt(pow((x - X[b]), 2) + pow((y - Y[b]), 2));   //分割點和炸彈的距離 
+						
+					if(R[b] - d > 0)
+					{
+						eachPathRisk += P[b] * (R[b] - d) / R[b];
+					}
 							
-			}
-	        }
+				}
+	        	}
+		}
 		
 		totalRisk += eachPathRisk;
 	}
