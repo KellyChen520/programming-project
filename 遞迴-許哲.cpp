4defa42ca9113e9e2ec98fa9d92b4bee7dfd187a
NerdIsMe <45210358+NerdIsMe@users.noreply.github.com>
2018-11-24 17:50:55 +0800

    Update 遞迴-許哲.cpp

--- 遞迴-許哲.cpp
+++ 遞迴-許哲.cpp
@@ -1,19 +1,21 @@
 float lowcost=risk0;//lowcost是不轉的風險 
 int *turnplace = new int [2*turntimes+5],ansturn=0;
-find(1,turntimes,inD,circleMinX, circleMaxX, circleMinY, circleMaxY, turnPlace,ansturn,ansXY);
+float distance =0;
+find(1,turntimes,inD,circleMinX, circleMaxX, circleMinY, circleMaxY, turnPlace,ansturn,ansXY,distance);
 
 
 void find(float d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
-				int circleMinY,,int circleMaxY,int* turnPlace,int &ansturn,int *ansXY)
+				int circleMinY,,int circleMaxY,int* turnPlace,int &ansturn,int *ansXY , float &distance)
 {//轉k次 	
-	float distance=0 ; 
 	if(k>turntimes)
 		return ;
 	for(int y = circleMinY; y <= circleMaxY; y++)
 	{
 		for(int x = circleMinX; x <= circleMaxX; x++)
 		{
-			
+			bool check = repeat ( x , y , k , turnPlace);
+			if ( check == 0 )
+				continue;
 			if(inD[y][x] == 1 )
 			{
 				turnPlace[2*k] = x;
@@ -32,11 +34,17 @@ void find(float d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 					for(int i=2;i<2*k+2;i++)
 						ansXY[i-2] = turnPlace[i];
 				}
-				find(d,k+1,turntimes,inD,circleMixX, circleMaxX, circleMixY, circleMaxY, turnPlace, ansturn, ansXY);		
+				find(d,k+1,turntimes,inD,circleMixX, circleMaxX, circleMixY, circleMaxY, turnPlace, ansturn, ansXY, distance);		
 			}
 		} 
 	}
 }
 
-
+	bool repeat(int x,int y, int k, int *turnPlace){
+		bool check=1;
+		for(int i=0;i<=k;i++)
+			if (x == turnPlace[2*i] && y == turnPlace[2*i+1] )
+				check=0;
+		return check;
+	}
 
