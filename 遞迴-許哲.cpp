377df8ea1ebdc5b251e02122fa625a15f68eb433
NerdIsMe <45210358+NerdIsMe@users.noreply.github.com>
2018-11-24 16:52:20 +0800

    Update 遞迴-許哲.cpp

--- 遞迴-許哲.cpp
+++ 遞迴-許哲.cpp
@@ -1,12 +1,12 @@
-float lowcost=risk0;//lowcost¬O¤£Âàªº­·ÀI 
+float lowcost=risk0;//lowcostæ˜¯ä¸è½‰çš„é¢¨éšª 
 int *turnplace = new int [2*turntimes+5],ansturn=0;
 find(1,turntimes,inD,circleMinX, circleMaxX, circleMinY, circleMaxY, turnPlace,ansturn);
 
 
 void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 				int circleMinY,,int circleMaxY,int* turnPlace,int &ansturn)
-{//Âàk¦¸ 	
-	int distance=0;
+{//è½‰kæ¬¡ 	
+	float distance=0;
 	if(k>turntimes)
 		return ;
 	for(int y = circleMinY; y <= circleMaxY; y++)
@@ -18,10 +18,12 @@ void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 			{
 				turnPlace[2*k] = x;
 				turnPlace[2*k+1] = y;
-				distance += sqrt(pow((turnPlace[2*k-2]-turnPlace[2*k]),2)+pow((turnPlace[2k-1]-turnPlace[2*k+1]),2));
-				if(distance>d)
+				float shortdis= sqrt(pow((turnPlace[2*turntimes+2]-turnPlace[2*k]),2)+pow((turnPlace[2*turntimes+3]-turnPlace[2*k+1]),2));
+				if(shortdis+distance>static_cast<float>(d)){ 
 					continue;
-				float cost= risk function(Âà§éÂI(x,y))+w(k-1);
+				}
+				distance+=shortdis;
+				float cost= risk function(è½‰æŠ˜é»(x,y))+w(k-1);
 				if(cost<lowcost){
 					ansturn=k;
 					lowcost=cost;
@@ -35,7 +37,6 @@ void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 	 
 	
 }
-
 
 
 
