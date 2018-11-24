a63764296f55c66a35a84e34328d2eed2d6897b4
NerdIsMe <45210358+NerdIsMe@users.noreply.github.com>
2018-11-24 17:34:34 +0800

    Update 遞迴-許哲.cpp

--- 遞迴-許哲.cpp
+++ 遞迴-許哲.cpp
@@ -1,12 +1,12 @@
 float lowcost=risk0;//lowcost是不轉的風險 
 int *turnplace = new int [2*turntimes+5],ansturn=0;
-find(1,turntimes,inD,circleMinX, circleMaxX, circleMinY, circleMaxY, turnPlace,ansturn);
+find(1,turntimes,inD,circleMinX, circleMaxX, circleMinY, circleMaxY, turnPlace,ansturn,ansXY);
 
 
-void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
+void find(float d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 				int circleMinY,,int circleMaxY,int* turnPlace,int &ansturn,int *ansXY)
 {//轉k次 	
-	float distance=0;
+	float distance=0 ; 
 	if(k>turntimes)
 		return ;
 	for(int y = circleMinY; y <= circleMaxY; y++)
@@ -18,15 +18,13 @@ void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 			{
 				turnPlace[2*k] = x;
 				turnPlace[2*k+1] = y;
-				float finaldis;
-				if(k==1)
-					finaldis = sqrt(pow((turnPlace[0]-turnPlace[2]),2)+pow((turnPlace[1]-turnPlace[3]),2));
-				if(k>=2)
-					finaldis= sqrt(pow((turnPlace[2*turntimes+2]-turnPlace[2*k]),2)+pow((turnPlace[2*turntimes+3]-turnPlace[2*k+1]),2));
-				if(finaldis+distance>static_cast<float>(d)){ 
+				float finalDis=0,tempDis=0;
+				tempDis = sqrt(pow((turnPlace[2*k]-turnPlace[2*k-2]),2)+pow((turnPlace[2*k+1]-turnPlace[2*k-1]),2));
+				finalDis= sqrt(pow((turnPlace[2*turntimes+2]-turnPlace[2*k]),2)+pow((turnPlace[2*turntimes+3]-turnPlace[2*k+1]),2));
+				if( finalDis+distance+tempDis > d){ 
 					continue;
 				}
-				distance+=finaldis;
+				distance+=tempDis;
 				float cost= risk function(轉折點(x,y))+w(k-1);
 				if(cost<lowcost){
 					ansturn=k;
@@ -34,13 +32,10 @@ void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 					for(int i=2;i<2*k+2;i++)
 						ansXY[i-2] = turnPlace[i];
 				}
-				find(d,k+1,turntimes,inD,circleMixX, circleMaxX, circleMixY circleMaxY, turnPlace, ansturn);
-						
+				find(d,k+1,turntimes,inD,circleMixX, circleMaxX, circleMixY, circleMaxY, turnPlace, ansturn, ansXY);		
 			}
 		} 
 	}
-	 
-	
 }
 
 
