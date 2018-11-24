dbbc2e3305df1ca028850f5a5e398de604dcd023
NerdIsMe <45210358+NerdIsMe@users.noreply.github.com>
2018-11-24 17:13:45 +0800

    Update 遞迴-許哲.cpp

--- 遞迴-許哲.cpp
+++ 遞迴-許哲.cpp
@@ -4,7 +4,7 @@ find(1,turntimes,inD,circleMinX, circleMaxX, circleMinY, circleMaxY, turnPlace,a
 
 
 void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
-				int circleMinY,,int circleMaxY,int* turnPlace,int &ansturn)
+				int circleMinY,,int circleMaxY,int* turnPlace,int &ansturn,int *ansXY)
 {//轉k次 	
 	float distance=0;
 	if(k>turntimes)
@@ -18,20 +18,21 @@ void find(int d,int k,int turntimes,int **inD,int circleMinX,int circleMaxX,
 			{
 				turnPlace[2*k] = x;
 				turnPlace[2*k+1] = y;
-				float shortdis;
+				float finaldis;
 				if(k==1)
-					shortdis = sqrt(pow((turnPlace[0]-turnPlace[2]),2)+pow((turnPlace[1]-turnPlace[3]),2));
+					finaldis = sqrt(pow((turnPlace[0]-turnPlace[2]),2)+pow((turnPlace[1]-turnPlace[3]),2));
 				if(k>=2)
-					shortdis= sqrt(pow((turnPlace[2*turntimes+2]-turnPlace[2*k]),2)+pow((turnPlace[2*turntimes+3]-turnPlace[2*k+1]),2));
-				if(shortdis+distance>static_cast<float>(d)){ 
+					finaldis= sqrt(pow((turnPlace[2*turntimes+2]-turnPlace[2*k]),2)+pow((turnPlace[2*turntimes+3]-turnPlace[2*k+1]),2));
+				if(finaldis+distance>static_cast<float>(d)){ 
 					continue;
 				}
-				distance+=shortdis;
+				distance+=finaldis;
 				float cost= risk function(轉折點(x,y))+w(k-1);
 				if(cost<lowcost){
 					ansturn=k;
 					lowcost=cost;
-					
+					for(int i=2;i<2*k+2;i++)
+						ansXY[i-2] = turnPlace[i];
 				}
 				find(d,k+1,turntimes,inD,circleMixX, circleMaxX, circleMixY circleMaxY, turnPlace, ansturn);
 						
